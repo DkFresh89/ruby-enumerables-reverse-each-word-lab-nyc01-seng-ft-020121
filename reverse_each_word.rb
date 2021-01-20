ff6d978c671ad2cd3c4b7a8dddeb08b76ced418f
Doug K <doug.kerzner@gmail.com>
2021-01-20 20:03:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 require 'pry' 
 
 def reverse_each_word(string)
-  string.split(" ") do
-   return string.reverse 
+  string.split(" ") do |string, index|
+   string.reverse
+   return index.reverse 
   end 
 end 
