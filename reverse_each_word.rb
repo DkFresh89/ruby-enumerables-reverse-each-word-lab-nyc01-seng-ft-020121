3b2871526b0fdc010f9beb464d829b2fc64ae04b
Doug K <doug.kerzner@gmail.com>
2021-01-20 20:25:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 require 'pry' 
 
 def reverse_each_word(string)
- myarray =  string.split do |sentence|
+ myarray = []  
+    string.split do |sentence|
     newarray.unshift (sentence.reverse)
   end 
   return newarray
