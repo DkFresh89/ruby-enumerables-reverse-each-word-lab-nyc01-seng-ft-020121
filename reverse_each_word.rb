c421466a5172517a02ab5f7de719ab2011eaca31
Doug K <doug.kerzner@gmail.com>
2021-01-20 20:24:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 require 'pry' 
 
 def reverse_each_word(string)
-  newarray = []
-  string.collect do |sentence|
+  newarray = string.shift do |sentence|
     newarray.unshift (sentence.reverse)
   end 
   return newarray
